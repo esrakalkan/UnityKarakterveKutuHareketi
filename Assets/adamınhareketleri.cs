b64889a539835ce9911e5888f7ffd7346ad240a9
esrakalkan <esrakalkan3000@gmail.com>
2019-12-19 14:26:44 +0300

    kutular,karakter,cevre olusturuldu.

---
+++ Assets/adamınhareketleri.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class adamınhareketleri : MonoBehaviour
+{
+    private float yatayDonme;
+    private float dikeyDonme;
+    public float hizDegeri = 5.0f;
+    public float donusDegeri = 50f;
+    private float dikeyEksen;
+    private float yatayEksen;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dikeyEksen = Input.GetAxis("Horizontal") * Time.deltaTime * hizDegeri;
+        yatayEksen = Input.GetAxis("Vertical") * Time.deltaTime * hizDegeri;
+        transform.Translate(dikeyEksen, 0.0f, yatayEksen);
+        yatayDonme = Input.GetAxis("Mouse X") * Time.deltaTime * donusDegeri;
+        transform.Rotate(0, yatayDonme, 0);
+
+
+        if (Input.GetKeyDown(KeyCode.Q) == true)
+        {
+            transform.Translate(0, 100*Time.deltaTime, 0);
+
+
+        }
+
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)  //otomatik çarptığım nesneyi bulur.
+    {
+
+        Debug.Log(collision.gameObject.name);
+    }
+
+
+}
---
+++ Assets/boloklar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boloklar : MonoBehaviour
+{
+    private float counter = 1;
+    private int rado = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter -= Time.deltaTime;
+
+        if (counter < 0)
+        {
+            rado = Random.Range(1, 4);
+            counter = 3;
+        }
+
+        if (rado == 1)
+        {
+            transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y, transform.position.z);
+        }
+        if (rado == 2)
+        {
+            transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y, transform.position.z);
+        }
+        if (rado == 3)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.01f);
+        }
+        if (rado == 4)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.01f);
+        }
+    }
+}
